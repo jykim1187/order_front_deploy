43581d418db393d8fbc458842c07a24b3ba524b6
jykim1187 <rifkehtm862@gmail.com>
2025-03-06 10:27:24 +0900

    test0306

--- src/components/HeaderComponent.vue
+++ src/components/HeaderComponent.vue
@@ -10,7 +10,7 @@
                         </div>
                     </v-col>
                     <v-col class="text-center">
-                        <v-btn :to="{path:'/'}">java shop 0305 test2</v-btn>
+                        <v-btn :to="{path:'/'}">java shop 0306 test</v-btn>
                     </v-col>
                     <v-col class="d-flex justify-end">
                         <v-btn v-if="isLogin" :to="{path:'/order/cart'}">장바구니 {{ getTotalQuantity }}</v-btn>
